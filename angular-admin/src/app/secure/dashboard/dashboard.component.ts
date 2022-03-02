9bce3be059f852e6ae2664b043a509417934f890
Alex Dita <alex.dita2207@gmail.com>
2022-03-02 19:46:56 +0000

    Chart facut

--- angular-admin/src/app/secure/dashboard/dashboard.component.ts
+++ angular-admin/src/app/secure/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as c3 from 'c3';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private orderService: OrderService) {
+  }
 
   ngOnInit(): void {
+    let chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        x: 'x',
+        columns: [
+          ['x'],
+          ['Sales'],
+        ],
+        types: {
+          Sales: 'bar'
+        }
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      }
+    });
+
+    this.orderService.chart().subscribe(
+      (result: { date: string, sum: number }[]) => {
+        chart.load({
+          columns: [
+            ['x', ...result.map(r => r.date)],
+            ['Sales', ...result.map(r => r.sum)]
+          ]
+        });
+      }
+    );
   }
 
 }
